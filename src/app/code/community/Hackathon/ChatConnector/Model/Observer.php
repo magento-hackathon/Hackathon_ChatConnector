a63defa9065f64fecbf303a91293430961f0fc35
Wouter Cleymans <wouter.cleymans@phpro.be>
2015-05-29 13:53:32 +0200

    Add class info

--- src/app/code/community/Hackathon/ChatConnector/Model/Observer.php
+++ src/app/code/community/Hackathon/ChatConnector/Model/Observer.php
@@ -1,9 +1,29 @@
 <?php
 
     /**
-     * Class Hackathon_ChatConnector_Model_Observer
+     * Hackathon_ChatConnector extension
+     *
+     * NOTICE OF LICENSE
+     *
+     * This source file is subject to the MIT License
+     * that is bundled with this package in the file LICENSE.txt.
+     * It is also available through the world-wide-web at this URL:
+     * http://opensource.org/licenses/mit-license.php
+     *
+     * @category       Hackathon
+     * @package        Hackathon_ChatConnector
+     * @copyright      Copyright (c) 2015
+     * @license        http://opensource.org/licenses/mit-license.php MIT License
      */
-    class Hackathon_ChatConnector_Model_Observer extends Mage_Core_Model_Observer
+
+    /**
+     * Abstract connector model
+     *
+     * @category    Hackathon
+     * @package     Hackathon_ChatConnector
+     * @author      Wouter Cleymans <wouter.cleymans@phpro.be>
+     */
+    class Hackathon_ChatConnector_Model_Observer
     {
 
         /**
