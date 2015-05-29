fdf202fc62cbbb7ce426902fdcb92be9286b0509
sander_mangel <mail.contentmedia@gmail.com>
2015-05-29 13:56:50 +0200

    [TASK] Moved sysconf getters to helper

--- src/app/code/community/Hackathon/ChatConnector/Helper/Data.php
+++ src/app/code/community/Hackathon/ChatConnector/Helper/Data.php
@@ -5,5 +5,43 @@
  */
 class Hackathon_ChatConnector_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    const XML_PATH_GENERAL_ENABLED = 'hackathon_chatconnector/general/enabled';
+    const XML_PATH_GENERAL_CONNECTORS = 'hackathon_chatconnector/general/connectors';
+    const XML_PATH_GENERAL_FREQUENCY = 'hackathon_chatconnector/general/retry_frequency';
 
+    /**
+     * Check if chat connector is active
+     *
+     * @param null $store
+     * @return bool
+     */
+    public function isActive($store = null)
+    {
+        return Mage::getStoreConfigFlag(self::XML_PATH_GENERAL_ENABLED, $store);
+    }
+
+    /**
+     * Retrieve all active chat connectors
+     *
+     * @param null $store
+     * @return array|mixed
+     */
+    public function getConnectors($store = null)
+    {
+        $connectors = Mage::getStoreConfig(self::XML_PATH_GENERAL_CONNECTORS, $store);
+        $connectors = explode(',', $connectors);
+
+        return $connectors;
+    }
+
+    /**
+     * Get retry frequency
+     *
+     * @param null $store
+     * @return int
+     */
+    public function getRetryFrequency($store = null)
+    {
+        return (int)Mage::getStoreConfig(self::XML_PATH_GENERAL_FREQUENCY, $store);
+    }
 }
