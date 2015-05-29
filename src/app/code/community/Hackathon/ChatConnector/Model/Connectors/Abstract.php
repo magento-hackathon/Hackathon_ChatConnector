534426bc45ee4bd05f47eab941cfb6b77a78f1fd
sander_mangel <mail.contentmedia@gmail.com>
2015-05-29 12:25:17 +0200

    [TASK] Interface and Abstract class for connectors

---
+++ src/app/code/community/Hackathon/ChatConnector/Model/Connectors/Abstract.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Hackathon_ChatConnector extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category       Hackathon
+ * @package        Hackathon_ChatConnector
+ * @copyright      Copyright (c) 2015
+ * @license        http://opensource.org/licenses/mit-license.php MIT License
+ */
+/**
+ * Abstract connector model
+ *
+ * @category    Hackathon
+ * @package     Hackathon_ChatConnector
+ * @author      Sander Mangel <sander@sandermangel.nl>
+ */
+class Hackathon_ChatConnector_Model_Connectors_Queue extends Mage_Core_Model_Abstract
+{
+
+    /**
+     * _processQueue
+     *
+     * @access protected
+     * @return Void
+     */
+    protected function _processQueue()
+    {
+    }
+}
