1256807106fb4013d0191c9a37541fb145bbef0e
Wouter Cleymans <wouter.cleymans@phpro.be>
2015-05-29 13:49:20 +0200

    Customer register success event

---
+++ src/app/code/community/Hackathon/ChatConnector/Model/Observer.php
@@ -0,0 +1,19 @@
+<?php
+
+    /**
+     * Class Hackathon_ChatConnector_Model_Observer
+     */
+    class Hackathon_ChatConnector_Model_Observer extends Mage_Core_Model_Observer
+    {
+
+        /**
+         * Listen to customer register success event
+         *
+         * @param $event
+         */
+        public function customerRegisterSuccess($event)
+        {
+
+        }
+
+    }
