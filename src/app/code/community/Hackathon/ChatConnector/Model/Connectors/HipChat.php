4ec188b21667eaa821960bda8efa677c1d8fe10f
Marcel Hauri <marcel@hauri.me>
2015-05-29 13:55:29 +0200

    add hipchat and slack connectors

---
+++ src/app/code/community/Hackathon/ChatConnector/Model/Connectors/HipChat.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Hackathon_ChatConnector extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category       Hackathon
+ * @package        Hackathon_ChatConnector
+ * @copyright      Copyright (c) 2015
+ * @license        http://opensource.org/licenses/mit-license.php MIT License
+ */
+/**
+ * HipChat Connector
+ *
+ * @category    Hackathon
+ * @package     Hackathon_ChatConnector
+ * @author      Marcel Hauri <marcel@hauri.me>
+ */
+
+class Hackathon_ChatConnector_Model_Connectors_HipChat
+    extends Hackathon_ChatConnector_Model_Connectors_Abstract
+{
+    public function getConfig()
+    {
+
+    }
+
+    public function notify()
+    {
+
+    }
+}
