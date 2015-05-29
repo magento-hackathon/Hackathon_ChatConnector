83f25cd731bb426deb37022062510996978b2b82
Marcel Hauri <marcel@hauri.me>
2015-05-29 13:36:35 +0200

    add sendNotification method with basic curl definitions

--- src/app/code/community/Hackathon/ChatConnector/Model/Connectors/Abstract.php
+++ src/app/code/community/Hackathon/ChatConnector/Model/Connectors/Abstract.php
@@ -19,7 +19,7 @@
  *
  * @category    Hackathon
  * @package     Hackathon_ChatConnector
- * @author      Sander Mangel <sander@sandermangel.nl>
+ * @author      Sander Mangel <sander@sandermangel.nl>, Marcel Hauri <marcel@hauri.me>
  */
 class Hackathon_ChatConnector_Model_Connectors_Queue extends Mage_Core_Model_Abstract
 {
@@ -32,5 +32,51 @@ class Hackathon_ChatConnector_Model_Connectors_Queue extends Mage_Core_Model_Abs
      */
     protected function _processQueue()
     {
+    }
+
+
+    /**
+     * @param array $params
+     * @param null $connector
+     * @return bool
+     */
+    public function sendNotification($params = array(), $connector = null)
+    {
+        // TODO: Check if ChatConnector is enabled
+
+        $url    = null;
+        $data   = json_encode($params);
+        $ch     = curl_init();
+
+        /**
+         * connector specific curl configuration
+         */
+        switch ($connector) {
+            case 'slack':
+                $data = array('payload' => json_encode($params));
+                break;
+            case 'hipchat':
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+                break;
+            default:
+                // TODO: Log error, or throw new exception
+                curl_close($ch);
+                return false;
+        }
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_NOBODY, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        if(!curl_exec($ch)) {
+            // TODO: Log error, or throw new exception
+            curl_close($ch);
+            return false;
+        }
+        curl_close($ch);
+        return true;
     }
 }
