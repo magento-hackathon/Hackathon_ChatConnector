a51b28f6b94e355c6a0099522b4257949bed0838
Wouter Cleymans <wouter.cleymans@phpro.be>
2015-05-29 15:33:36 +0200

    Fix merge conflict

--- src/app/code/community/Hackathon/ChatConnector/Model/Events/Customer/Register.php
+++ src/app/code/community/Hackathon/ChatConnector/Model/Events/Customer/Register.php
@@ -1,53 +1,48 @@
 <?php
-/**
- * Hackathon_ChatConnector extension
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the MIT License
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/mit-license.php
- *
- * @category       Hackathon
- * @package        Hackathon_ChatConnector
- * @copyright      Copyright (c) 2015
- * @license        http://opensource.org/licenses/mit-license.php MIT License
- */
-
-/**
- * Abstract connector model
- *
- * @category    Hackathon
- * @package     Hackathon_ChatConnector
- * @author      Wouter Cleymans <wouter.cleymans@phpro.be>
- */
-class Hackathon_ChatConnector_Model_Observer
-{
+    /**
+     * Hackathon_ChatConnector extension
+     *
+     * NOTICE OF LICENSE
+     *
+     * This source file is subject to the MIT License
+     * that is bundled with this package in the file LICENSE.txt.
+     * It is also available through the world-wide-web at this URL:
+     * http://opensource.org/licenses/mit-license.php
+     *
+     * @category       Hackathon
+     * @package        Hackathon_ChatConnector
+     * @copyright      Copyright (c) 2015
+     * @license        http://opensource.org/licenses/mit-license.php MIT License
+     */
 
     /**
-     * Listen to customer register success event
+     * Customer register event
      *
-     * @param Varien_Event_Observer $event
+     * @category    Hackathon
+     * @package     Hackathon_ChatConnector
+     * @author      Wouter Cleymans <wouter.cleymans@phpro.be>
      */
-    public function customerRegisterSuccess(Varien_Event_Observer $event)
+    class Hackathon_ChatConnector_Model_Events_Customer_Register extends Hackathon_ChatConnector_Model_Events_Abstract
+        implements Hackathon_ChatConnector_Model_Events_Interface
     {
-        $customer = $event->getCustomer();
-        $accountController = $event->getAccountController();
 
-        // Start logging!
+        /**
+         * Listen to customer register success event
+         *
+         * @param Varien_Event_Observer $event
+         */
+        public function customerRegisterSuccess(Varien_Event_Observer $event)
+        {
+            $customer          = $event->getCustomer();
+            $accountController = $event->getAccountController();
+
+            // Start logging!
+            Mage::log('customer register success');
+        }
+
+        public function listener()
+        {
+            // TODO: Implement listener() method.
+        }
+
     }
-
-    /**
-     * Listen to order cancel payment event
-     *
-     * @param Varien_Event_Observer $event
-     */
-    public function salesOrderPaymentCancel(Varien_Event_Observer $event)
-    {
-        $payment = $event->getPayment();
-
-        // Start logging!
-    }
-
-}
