40478cd2cefd6ab132d1779f097458138a2187d9
Marcel Hauri <marcel@hauri.me>
2015-05-29 14:48:20 +0200

    update connectors

--- src/app/code/community/Hackathon/ChatConnector/Model/Connectors/HipChat.php
+++ src/app/code/community/Hackathon/ChatConnector/Model/Connectors/HipChat.php
@@ -25,13 +25,43 @@
 class Hackathon_ChatConnector_Model_Connectors_HipChat
     extends Hackathon_ChatConnector_Model_Connectors_Abstract
 {
+    /**
+     * @return array
+     */
     public function getConfig()
     {
-
+        $store = Mage::app()->getStore()->getStoreId();
+        return array(
+            'access_token' => Mage::getStoreConfig('hackathon_chatconnector/hipchat/access_token', $store),
+            'room_id'      => Mage::getStoreConfig('hackathon_chatconnector/hipchat/room_id', $store),
+        );
     }
 
-    public function notify()
+    public function notify($params = array())
     {
+        $config = $this->getConfig();
 
+        $url = sprintf('http://api.hipchat.com/v2/room/%d/notification?auth_token=%s',
+            intval($config['room_id']),
+            $config['access_token']
+        );
+
+        $ch     = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_NOBODY, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_exec($ch);
+
+        $status = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if(intval($status) === 200 || intval($status) === 204) {
+            return true;
+        }
+        return false;
     }
 }
--- src/app/code/community/Hackathon/ChatConnector/Model/Connectors/Slack.php
+++ src/app/code/community/Hackathon/ChatConnector/Model/Connectors/Slack.php
@@ -25,13 +25,43 @@
 class Hackathon_ChatConnector_Model_Connectors_Slack
     extends Hackathon_ChatConnector_Model_Connectors_Abstract
 {
+    /**
+     * @return array
+     */
     public function getConfig()
     {
-
+        $store = Mage::app()->getStore()->getStoreId();
+        return array(
+            'webhook_url' => Mage::getStoreConfig('hackathon_chatconnector/slack/webhook_url', $store),
+            'channel'     => Mage::getStoreConfig('hackathon_chatconnector/slack/channel', $store),
+            'username'    => Mage::getStoreConfig('hackathon_chatconnector/slack/username', $store),
+            'icon'        => Mage::getStoreConfig('hackathon_chatconnector/slack/icon', $store),
+        );
     }
 
-    public function notify()
+    /**
+     * @param array $params
+     * @return bool
+     */
+    public function notify($params = array())
     {
+        $config = $this->getConfig();
 
+        $ch     = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $config['webhook_url']);
+        curl_setopt($ch, CURLOPT_NOBODY, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array('payload' => json_encode($params)));
+        curl_exec($ch);
+
+        $status = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if(intval($status) === 200) {
+            return true;
+        }
+        return false;
     }
 }
